8ed1e06d2a2d44470b75c42fa16c7d937788807f
scrawach <scrawach@gmail.com>
2024-02-15 23:29:49 +0300

    added test window open

---
+++ src/UnityWwiseProject/Assets/CodeBase/TestWindowOpen.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace CodeBase
+{
+    public static class TestWindowOpen
+    {
+        [MenuItem("Test/Open")]
+        public static void Open()
+        {
+            Debug.Log($"OPEN!");
+        }
+    }
+}
