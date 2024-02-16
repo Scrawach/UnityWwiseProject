cb405db34664e358bd576d9be398e5be1cdcabbf
scrawach <scrawach@gmail.com>
2024-02-16 19:48:21 +0300

    implemented test window with property field

---
+++ src/UnityWwiseProject/Assets/CodeBase/TestWindow.cs
@@ -0,0 +1,52 @@
+﻿using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Event = AK.Wwise.Event;
+
+namespace CodeBase
+{
+    public class TestWindow : EditorWindow
+    {
+        public AK.Wwise.Event _event = null;
+        
+        private void CreateGUI()
+        {
+            DrawInspector();
+        }
+
+        private void DrawInspector()
+        {
+            var root = rootVisualElement;
+            var temp = new SerializedObject(this);
+            var property = temp.FindProperty("_event");
+            var propertyField = new PropertyField(property);
+            propertyField.Bind(temp);
+            root.Add(propertyField);
+
+            var button = new Button(OnButtonClicked)
+            {
+                text = "Play"
+            };
+            root.Add(button);
+        }
+
+        private void OnButtonClicked()
+        {
+            Play(_event);
+            //AkWaapiUtilities.TogglePlayEvent(WwiseObjectType.Event, _event.WwiseObjectReference.Guid);
+        }
+
+        private async void Play(Event sound)
+        {
+            var global = Resources.Load<AkInitializer>("WwiseGlobal");
+            var instantiate = GameObject.Instantiate(global);
+            AkSoundEngineController.Instance.Init(instantiate);
+            Debug.Log($"{global}");
+            var result = await AkBankManagerAsync.LoadBankAsync("Main");
+            Debug.Log($"Result {result.Result}");
+            _event.Post(instantiate.gameObject);
+            Debug.Log($"POST");
+        }
+    }
+}
--- src/UnityWwiseProject/Assets/CodeBase/TestWindowOpen.cs
+++ src/UnityWwiseProject/Assets/CodeBase/TestWindowOpen.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using UnityEditor;
-using UnityEngine;
+﻿using UnityEditor;
 
 namespace CodeBase
 {
     public static class TestWindowOpen
     {
         [MenuItem("Test/Open")]
-        public static void Open()
-        {
-            const string testSoundGuid = "63074727-acd1-4754-a758-15a77b458b62";
-            Debug.Log($"OPEN!");
-            AkWaapiUtilities.TogglePlayEvent(WwiseObjectType.Event, Guid.Parse(testSoundGuid));
-        }
+        public static void OpenWindow() =>
+            EditorWindow.GetWindow<TestWindow>(nameof(TestWindow));
     }
 }
