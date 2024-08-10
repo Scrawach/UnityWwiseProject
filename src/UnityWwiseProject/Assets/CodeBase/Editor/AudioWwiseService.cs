52755cb3a0e2ecb7d8b0cb2989095fc23c9be939
scrawach <scrawach@gmail.com>
2024-08-10 21:05:13 +0300

    implemented separated audio wwise service

---
+++ src/UnityWwiseProject/Assets/CodeBase/Editor/AudioWwiseService.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CodeBase.Editor
+{
+    public class AudioWwiseService
+    {
+        private const string EventGuid = "63074727-ACD1-4754-A758-15A77B458B62";
+        private const uint EventId = 1784581504;
+        
+        private readonly List<AkBankManagerAsync.AsyncLoadingBankResult> _banks;
+
+        private GameObject _audioListener;
+
+        public AudioWwiseService() => 
+            _banks = new List<AkBankManagerAsync.AsyncLoadingBankResult>();
+
+        public async void Initialize()
+        {
+            var akInitializers = Resources.Load<AkInitializer>("WwiseGlobal");
+            AkSoundEngineController.Instance.Init(akInitializers);
+            AkSoundEngine.RegisterGameObj(akInitializers.gameObject);
+            
+            if (!IsBankLoaded("Main")) 
+                await LoadBank("Main");
+
+            _audioListener = akInitializers.gameObject;
+        }
+        
+        public float GetDuration(uint playingId)
+        {
+            var eventId = AkSoundEngine.GetEventIDFromPlayingID(playingId);
+            var data = AkWwiseProjectInfo.GetData().GetEventInfo(eventId);
+            var result = AkSoundEngine.GetSourcePlayPosition(playingId, out var playPosition);
+            var playPositionInSeconds = playPosition / 1000f;
+
+            if (data == null)
+                return 0f;
+            
+            return playPositionInSeconds / data.maxDuration;
+        }
+        
+        public uint Play()
+        {
+            var flags = (uint)AkCallbackType.AK_MusicSyncAll 
+                        | (uint)AkCallbackType.AK_EnableGetMusicPlayPosition 
+                        | (uint)AkCallbackType.AK_EnableGetSourcePlayPosition
+                        | (uint)AkCallbackType.AK_EnableGetSourceStreamBuffering 
+                        | (uint)AkCallbackType.AK_CallbackBits;
+            
+            return AkSoundEngine.PostEvent(EventId, _audioListener, flags, OnCallback, null);
+        }
+        
+        private void OnCallback(object in_cookie, AkCallbackType in_type, AkCallbackInfo in_info)
+        {
+            Debug.Log($"Callback: {in_info.gameObjID}");
+        }
+        
+        private async Task LoadBank(string bankName)
+        {
+            var result = await AkBankManagerAsync.LoadBankAsync(bankName);
+            _banks.Add(result);
+            Debug.Log($"Load bank {bankName} with result {result.Result}");
+        }
+
+        private bool IsBankLoaded(string bankName) =>
+            _banks.Any(bank => bank.Name == bankName);
+    }
+}
--- src/UnityWwiseProject/Assets/CodeBase/Editor/EditorWwisePlayerWindow.cs
+++ src/UnityWwiseProject/Assets/CodeBase/Editor/EditorWwisePlayerWindow.cs
@@ -1,47 +1,26 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using UnityEditor;
-using UnityEditor.UIElements;
+﻿using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
-using Event = AK.Wwise.Event;
 
 namespace CodeBase.Editor
 {
     public class EditorWwisePlayerWindow : EditorWindow
     {
-        public AK.Wwise.Event _event = null;
-
-        private const string EventGuid = "63074727-ACD1-4754-A758-15A77B458B62";
-        private const uint EventId = 1784581504;
-
-        private static List<AkBankManagerAsync.AsyncLoadingBankResult> _banks = new();
-
-        private static uint _playingId;
+        private AudioWwiseService _wwiseSerice = new();
+        private uint _playingId;
 
         private void CreateGUI()
         {
             DrawInspector();
+            _wwiseSerice.Initialize();
         }
 
         private void Update()
         {
-            if (_playingId != 0)
-            { 
-                PrintDuration(_playingId);
-            }
+            if (_playingId != 0) 
+                Debug.Log($"Progress: {_wwiseSerice.GetDuration(_playingId)}");
         }
-
-        private void PrintDuration(uint playingId)
-        {
-            var data = AkWwiseProjectInfo.GetData().GetEventInfo(EventId);
-            var result = AkSoundEngine.GetSourcePlayPosition(playingId, out var playPosition);
-            var playPositionInSeconds = playPosition / 1000f;
-            var progress = playPositionInSeconds / data.maxDuration;
-            Debug.Log($"{result}: {progress}");
-        }
-
+        
         private void DrawInspector() => 
             rootVisualElement.Add(UserButtons());
 
@@ -58,47 +37,10 @@ namespace CodeBase.Editor
             return elements;
         }
 
-        private void OnPauseButtonClicked()
-        {
+        private void OnPauseButtonClicked() => 
             AkSoundEngine.StopAll();
-        }
 
-        private void OnButtonClicked()
-        {
-            Play(_event);
-        }
-
-        private async void Play(Event sound)
-        {
-            var akInitializers = Resources.Load<AkInitializer>("WwiseGlobal");
-            AkSoundEngineController.Instance.Init(akInitializers);
-            AkSoundEngine.RegisterGameObj(akInitializers.gameObject);
-            if (!IsBankLoaded("Main")) 
-                await LoadBank("Main");
-
-            var flags = (uint)AkCallbackType.AK_MusicSyncAll 
-                        | (uint)AkCallbackType.AK_EnableGetMusicPlayPosition 
-                        | (uint)AkCallbackType.AK_EnableGetSourcePlayPosition
-                        | (uint)AkCallbackType.AK_EnableGetSourceStreamBuffering 
-                        | (uint)AkCallbackType.AK_CallbackBits;
-            Debug.Log($"{flags}");
-            
-            _playingId = AkSoundEngine.PostEvent(EventId, akInitializers.gameObject, flags, OnCallback, null);
-        }
-
-        private void OnCallback(object in_cookie, AkCallbackType in_type, AkCallbackInfo in_info)
-        {
-            Debug.Log($"Callback: {in_info.gameObjID}");
-        }
-
-        private async Task LoadBank(string bankName)
-        {
-            var result = await AkBankManagerAsync.LoadBankAsync(bankName);
-            _banks.Add(result);
-            Debug.Log($"Load bank {bankName} with result {result.Result}");
-        }
-
-        private bool IsBankLoaded(string bankName) =>
-            _banks.Any(bank => bank.Name == bankName);
+        private void OnButtonClicked() => 
+            _playingId = _wwiseSerice.Play();
     }
 }
