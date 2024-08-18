cd677d59b1d6fb9933d99de8a161056684937191
scrawach <scrawach@gmail.com>
2024-08-18 15:45:54 +0300

    moved all logic from window to root

--- src/UnityWwiseProject/Assets/CodeBase/Editor/Windows/WwisePlayerRoot.cs
+++ src/UnityWwiseProject/Assets/CodeBase/Editor/Windows/WwisePlayerRoot.cs
@@ -1,4 +1,5 @@
 using CodeBase.Editor.Services;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace CodeBase.Editor.Windows
@@ -6,12 +7,46 @@ namespace CodeBase.Editor.Windows
     public class WwisePlayerRoot : VisualElement
     {
         private readonly WwisePlayerWindow _parent;
+        private readonly AudioWwiseService _wwiseService;
+
+        private uint _playingId;
 
         public WwisePlayerRoot(WwisePlayerWindow parent)
         {
             _parent = parent;
             var eventsProvider = new AudioEventsProvider();
             var searchWindowProvider = new SearchWindowProvider(parent, eventsProvider);
+            _wwiseService = new AudioWwiseService();
+            _parent.rootVisualElement.Add(CreateButtons());
         }
+
+        public void Initialize()
+        {
+            _wwiseService.Initialize();
+        }
+
+        public void Update()
+        {
+            Debug.Log($"Progress: {_wwiseService.GetDuration(_playingId)}");
+        }
+
+        private VisualElement CreateButtons()
+        {
+            var elements = new VisualElement();
+
+            var playButton = new Button(OnButtonClicked) { text = "Play" };
+            var stopButton = new Button(OnPauseButtonClicked) { text = "Stop" };
+            
+            elements.Add(playButton);
+            elements.Add(stopButton);
+
+            return elements;
+        }
+
+        private void OnPauseButtonClicked() => 
+            AkSoundEngine.StopAll();
+
+        private void OnButtonClicked() => 
+            _playingId = _wwiseService.Play();
     }
 }
--- src/UnityWwiseProject/Assets/CodeBase/Editor/Windows/WwisePlayerWindow.cs
+++ src/UnityWwiseProject/Assets/CodeBase/Editor/Windows/WwisePlayerWindow.cs
@@ -7,41 +7,15 @@ namespace CodeBase.Editor.Windows
 {
     public class WwisePlayerWindow : EditorWindow
     {
-        private AudioWwiseService _wwiseSerice = new();
-        private uint _playingId;
-
+        private WwisePlayerRoot _root;
+        
         private void CreateGUI()
         {
-            DrawInspector();
-            _wwiseSerice.Initialize();
+            _root = new WwisePlayerRoot(this);
+            _root.Initialize();
         }
 
-        private void Update()
-        {
-            if (_playingId != 0) 
-                Debug.Log($"Progress: {_wwiseSerice.GetDuration(_playingId)}");
-        }
-        
-        private void DrawInspector() => 
-            rootVisualElement.Add(UserButtons());
-
-        private VisualElement UserButtons()
-        {
-            var elements = new VisualElement();
-
-            var playButton = new Button(OnButtonClicked) { text = "Play" };
-            var stopButton = new Button(OnPauseButtonClicked) { text = "Stop" };
-            
-            elements.Add(playButton);
-            elements.Add(stopButton);
-
-            return elements;
-        }
-
-        private void OnPauseButtonClicked() => 
-            AkSoundEngine.StopAll();
-
-        private void OnButtonClicked() => 
-            _playingId = _wwiseSerice.Play();
+        private void Update() => 
+            _root.Update();
     }
 }
